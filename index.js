fe9a75f27b7f08b01937efbf21c498bb7a206bef
Harsh-Kumar-art <hashkumar1506@gmail.com>
2022-09-26 18:39:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,191 @@
+const {Engine,Render,Runner,World,Bodies,Body,Events}=Matter;
+
+const cellsHorizontal=14;
+const cellsVertical=10;
+const width=window.innerWidth;
+const height=window.innerHeight;
+
+const unitLengthY=width/cellsHorizontal;
+const unitLengthX=width/cellsVertical;
+const engine=Engine.create();
+engine.world.gravity.y=0;
+const {world}=engine;
+const render=Render.create({
+    element:document.body,
+    engine:engine,
+    options:{
+        wireframes:false,
+        width,
+        height
+    }
+});
+Render.run(render);
+Runner.run(Runner.create(),engine);
+
+//Walls
+const walls=[
+    Bodies.rectangle(width/2,0,width,2,{isStatic:true}),
+    Bodies.rectangle(width/2,height,width,2,{isStatic:true}),
+    Bodies.rectangle(0,height/2,2,height,{isStatic:true}),
+    Bodies.rectangle(width,height/2,2,height,{isStatic:true})
+];
+World.add(world,walls);
+
+//Maze generation
+const shuffle=(arr)=>{
+    let counter=arr.length;
+    while(counter>0){
+        const ind=Math.floor(Math.random()*counter);
+        counter--;
+        const temp=arr[counter];
+        arr[counter]=arr[ind];
+        arr[ind]=temp;
+    }
+    return arr;
+}
+const grid=Array(cellsVertical).fill(null).map(()=>Array(cellsHorizontal).fill(false));
+const verticals=Array(cellsVertical).fill(null).map(()=>Array(cellsHorizontal-1).fill(false));
+const horizontals=Array(cellsVertical-1).fill(null).map(()=>Array(cellsHorizontal).fill(false));
+
+const startRow=Math.floor(Math.random()*cellsVertical);
+const startColumn=Math.floor(Math.random()*cellsHorizontal);
+
+const stepThroughCell=(row,columm)=>{
+    if(grid[row][columm])return;
+    grid[row][columm]=true;
+
+    const neighbours=shuffle([
+        [row-1,columm,'up'],
+        [row,columm+1,'right'],
+        [row+1,columm,'down'],
+        [row,columm-1,'left']
+    ]);
+
+    for(let neighbour of neighbours){
+        const [nextRow,nextColumn,direction]=neighbour;
+
+        if(nextRow<0 || nextRow>=cellsVertical || nextColumn<0 || nextColumn>=cellsHorizontal)continue;
+
+        if(grid[nextRow][nextColumn])continue;
+
+        if(direction==='left'){
+            verticals[row][columm-1]=true;
+        }
+        else if(direction==='right'){
+            verticals[row][columm]=true;
+        }
+        else if(direction==='up'){
+            horizontals[row-1][columm]=true;
+        }
+        else if(direction==='down'){
+            horizontals[row][columm]=true;
+        }
+
+        stepThroughCell(nextRow,nextColumn);
+    }
+    
+};
+
+stepThroughCell(startRow,startColumn);
+
+horizontals.forEach((row,rowInd)=>{
+    row.forEach((open,colummInd)=>{
+        if(open)return;
+        const wall=Bodies.rectangle(
+            colummInd*unitLengthX+unitLengthX/2,
+            rowInd*unitLengthY+unitLengthY,
+            unitLengthX,
+            5,
+            {isStatic:true,
+            label:'wall',
+            render:{
+                fillStyle:'red'
+            }}
+        );
+        World.add(world,wall);
+    });
+});
+verticals.forEach((row,rowInd)=>{
+    row.forEach((open,colummInd)=>{
+        if(open)return;
+        const wall=Bodies.rectangle(
+            colummInd*unitLengthX+unitLengthX,
+            rowInd*unitLengthY+unitLengthY/2,
+            10,
+            unitLengthY,
+            {isStatic:true,
+            label:'wall',
+            render:{
+                fillStyle:'red'
+            }}
+        
+            
+        );
+        World.add(world,wall);
+    });
+});
+
+
+//Goal
+const goal=Bodies.rectangle(
+    width-unitLengthX/2,
+    height-unitLengthY/2,
+    unitLengthX*(.7),
+    unitLengthY*(.7),
+    {
+        label:'goal',
+        isStatic:true,
+        render:{
+            fillStyle:'green'
+        }
+    }
+);
+World.add(world,goal);
+
+//Ball
+const ballRadius=Math.min(unitLengthX,unitLengthY)/4;
+const ball=Bodies.circle(
+    unitLengthX/2,
+    unitLengthY/2,
+    ballRadius,
+    {
+        label:'ball',
+        render:{
+            fillStyle:'blue'
+        }
+    }
+);
+World.add(world,ball);
+
+document.addEventListener('keydown',event=>{
+    const {x,y}=ball.velocity;
+    if(event.keyCode===87){
+        Body.setVelocity(ball,{x,y:y-5});
+    }
+    if(event.keyCode===68){
+        Body.setVelocity(ball,{x:x+5,y});
+    }
+    if(event.keyCode===83){
+        Body.setVelocity(ball,{x,y:y+5});
+    }
+    if(event.keyCode===65){
+        Body.setVelocity(ball,{x:x-5,y});
+    }
+});
+
+//Win Condition
+
+Events.on(engine,'collisionStart',event=>{
+    event.pairs.forEach(collision=>{
+        const labels=['ball','goal'];
+
+        if(labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)){
+            world.gravity.y=1;
+            world.bodies.forEach(body=>{
+                if(body.label==='wall'){
+                    Body.setStatic(body,false);
+                }
+            });
+        }
+    });
+});
